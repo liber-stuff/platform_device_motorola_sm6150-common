3a5fe35929026f49afa344f3978b1bb2a5c15519
Tao Bao <tbao@google.com>
2019-05-06 22:42:02 -0700

    sm6150-common: AB_OTA_UPDATER is BoardConfig variable.
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- common.mk
+++ common.mk
@@ -29,15 +29,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 # A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vendor \
-    vbmeta
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
