56396ce191f6aeff03db4172fa996602aefe80c8
menorziin <williamsilvapppl@gmail.com>
2022-08-18 00:03:30 -0300

    sm6150-common: build android.frameworks.sensorservice@1.0 HAL
    * E init : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 516 (/system/bin/hwservicemanager)
    
    Change-Id: I5a4feba0812bcbb187ef8a1c2298f799509e6c92

--- common.mk
+++ common.mk
@@ -432,6 +432,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 # Servicetracker
